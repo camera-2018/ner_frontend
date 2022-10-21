e756bb0c86b47b4a5b41c62584f57ba0b06b087d
camera-2018 <2907618001@qq.com>
2022-10-21 11:22:07 +0800

    fix: update api

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -24,7 +24,7 @@ const cleanText = () => {
 const goNER = async() => {
   changeLoading(true)
 
-  const url = isNewApi.value ? 'http://xyxsw.ltd:3539/api/ner' : 'http://xyxsw.ltd:3539/api/ner'
+  const url = isNewApi.value ? 'http://apiner.xyxsw.ltd/api/ner' : 'http://apiner.xyxsw.ltd/api/ner'
   if (!text.value || text.value.replace(/\s+/g, '').length === 0) {
     await changeLoading(false)
     alert('请输入文本')
